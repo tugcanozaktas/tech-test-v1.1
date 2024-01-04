1d22b4263318d6b66ecc295005ca52193e857779
Tuğcan Özaktaş <tugcanozaktas@hotmail.com>
2024-01-04 12:03:17 +0200

    saving search input into component's state

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
+import { useState } from 'react';
 import '../styles/App.css';
 import Search from './Search';
 
 function App() {
+  const [searchResults, setSearchResults] = useState(null);
   return (
     <div className="App">
       <img src='https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg' alt='nasa-logo' className='nasa-logo' />
-      <Search />
+      <Search setSearchResults={setSearchResults} />
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,18 @@
 import React, {useState} from "react";
 import "../styles/Search.css";
+import getImages from "../requests/getImages";
 
-const Search = () => {
+const Search = ({setSearchResults}) => {
     const [value, setValue] = useState(null);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setSearchResults(getImages(value));
+    }
 
     return(
       <div className="search">
-        <form className="search-form">
+        <form onSubmit={handleSubmit} className="search-form">
             <input className="search-input"  type="text" onChange={(e) => setValue(e.target.value)}/>
             <button className="search-button"  type="submit">Search!</button>
         </form>
