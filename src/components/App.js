813d34cfddb30a04a57db958fb0e1323db487ba8
Tuğcan Özaktaş <tugcanozaktas@hotmail.com>
2024-01-04 11:39:26 +0200

    created nasa logo and styled it

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import '../styles/App.css';
 function App() {
   return (
     <div className="App">
+      <img src='https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg' alt='nasa-logo' className='nasa-logo' />
     </div>
   );
 }
